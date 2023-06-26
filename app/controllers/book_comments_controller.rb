0f2e3161cdb4b00be4bf4fffd435c1bed4500d85
ogi0116 <135587780+ogi0116@users.noreply.github.com>
2023-06-26 14:16:22 +0000

    [Finish]応用課題3:コメント・いいね機能の追加

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,27 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    book = Book.find(params[:book_id])
+    comment = BookComment.new(book_comment_params)
+    comment.user_id = current_user.id
+    comment.book_id = book.id
+    comment.save
+    redirect_to book_path(book)
+  end
+
+#params[:id]をparams[:book_id]に変えた
+  def destroy
+    comment = BookComment.find(params[:book_id])
+    comment.destroy
+    redirect_to book_path(params[:id])
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+end
+
+
