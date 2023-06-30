a523497bc099c73f18395182e70d22aaa10b3a49
ogi0116 <135587780+ogi0116@users.noreply.github.com>
2023-06-30 13:20:57 +0000

    [Finish]応用課題6:bookers2いいねコメント機能を非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,20 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    #redirect_to book_path(book)
   end
 
 #destroyのルーティングを見ると2つ必要であるとわかったため
   def destroy
-    comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    comment.destroy
-    redirect_back fallback_location: root_path
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @comment.destroy
+   # redirect_back fallback_location: root_path
   end
 
   private
